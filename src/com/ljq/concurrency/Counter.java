3e963ab705afc9d4c4031be9a32f762935e80eb9
laijunquan <992807043@qq.com>
2019-06-24 23:47:18 +0800

    实现基于CAS线程安全的计数器

---
+++ src/com/ljq/concurrency/Counter.java
@@ -0,0 +1,59 @@
+package com.ljq.concurrency;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 计数器
+ */
+public class Counter {
+    private AtomicInteger atomicI = new AtomicInteger(0);
+    private int i = 0;
+
+    public static void main(String[] args) {
+        final Counter cas = new Counter();
+        List<Thread> ts = new ArrayList<>(600);
+        long start = System.currentTimeMillis();
+        for (int j = 0; j < 100; j++) {
+            Thread t = new Thread(() -> {
+                for (int i = 0; i < 10000; i++) {
+                    cas.count();
+                    cas.safaCount();
+                }
+            });
+            ts.add(t);
+        }
+
+        for (Thread t : ts) {
+            t.start();
+        }
+
+        for (Thread t : ts) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(cas.i);
+        System.out.println(cas.atomicI.get());
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+    private void safaCount() {
+        for (;;) {
+            int i = atomicI.get();
+            boolean suc = atomicI.compareAndSet(i, ++i);
+            if (suc) {
+                break;
+            }
+        }
+    }
+
+    private void count() {
+        i++;
+    }
+}
